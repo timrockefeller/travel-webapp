0ac3f34d3f321176ea01398fc4b67a9e33806091
xuchenyangniubi <1196758906@qq.com>
2019-07-17 22:53:20 +0800

    add controller

--- src/main/java/edu/ustb/controller/UserController.java
+++ src/main/java/edu/ustb/controller/UserController.java
@@ -24,7 +24,61 @@ public class UserController extends BaseServlet {
 
     //声明UserService业务对象
     private UserService service = new UserServiceImpl();
+    //登录回显
+    public void findback(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+         //从session中获取登录用户
+         Object user = request.getSession().getAttribute("user");
+         //将user写回客户端
+         ObjectMapper mapper = new ObjectMapper();
+         response.setContentType("application/json;charset=utf-8");
+         mapper.writeValue(response.getOutputStream(),user);
+    }
+    //登录
+    public void login(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        Map<String, String[]> map = request.getParameterMap();
+        //2.封装User对象
+        User user = new User();
+        try {
+            BeanUtils.populate(user,map);
+        } catch (IllegalAccessException e) {
+            e.printStackTrace();
+        } catch (InvocationTargetException e) {
+            e.printStackTrace();
+        }
 
+        //3.调用Service查询
+        UserService service = new UserServiceImpl();
+        User u  = service.login(user);
+
+        ResultInfo info = new ResultInfo();
+
+        //4.判断用户对象是否为null
+        if(u == null){
+            //用户名密码或错误
+            info.setFlag(false);
+            info.setErrorMsg("用户名密码或错误");
+        }
+        //5.判断用户是否激活
+        if(u != null && !"Y".equals(u.getStatus())){
+            //用户尚未激活
+            info.setFlag(false);
+            info.setErrorMsg("您尚未激活，请激活");
+        }
+        //6.判断登录成功
+        if(u != null && "Y".equals(u.getStatus())){
+      request.getSession().setAttribute("user",u);//登录成功标记
+
+            //登录成功
+            info.setFlag(true);
+        }
+
+        //响应数据
+        ObjectMapper mapper = new ObjectMapper();
+
+        response.setContentType("application/json;charset=utf-8");
+        mapper.writeValue(response.getOutputStream(),info);
+
+    }
     /**
      * 注册功能
      * @param request
